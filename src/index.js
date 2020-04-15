cf8b210d0343c52fb1d6886ac0b470a9ff07a4ed
Pedro Young <pedroyoung@livenation.com>
2020-04-14 19:35:12 -0700

    Updates

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
-console.info('Starter Site');                              
-                                
+export default function main() {
+  // eslint-disable-next-line no-console
+  console.info('Starter Site')
+}
